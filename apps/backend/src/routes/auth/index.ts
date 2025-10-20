f455db3788d5193306dabaec32ac3342ac2ce518
xcfio <119097812+xcfio@users.noreply.github.com>
2025-10-20 09:20:36 +0600

    fix issues

--- apps/backend/src/routes/auth/index.ts
+++ apps/backend/src/routes/auth/index.ts
@@ -1,6 +1,3 @@
-import AuthGithub from "./github"
 import { main } from "../../"
 
-export default function OAuth2(fastify: Awaited<ReturnType<typeof main>>) {
-    AuthGithub(fastify)
-}
+export default function OAuth2(fastify: Awaited<ReturnType<typeof main>>) {}
